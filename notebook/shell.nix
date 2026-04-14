204c74a5e7dad9776ad23ab31725938d08f32bae
alexander svensson <gurkslask@gmail.com>
2026-04-14 09:08:03 +0200

    Made a shell, that also install turtle for notebooks

---
+++ notebook/shell.nix
@@ -0,0 +1,32 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+
+in
+
+pkgs.mkShellNoCC {
+  packages = with pkgs; [
+    cowsay
+    lolcat
+    uv
+    python3
+    jupyter
+  ];
+  # Automatically run jupyter when entering the shell.
+  shellHook = ''
+  # 1. Skapa en virtuell miljö om den inte finns
+    if [ ! -d ".venv" ]; then
+      uv venv
+    fi
+    
+    # 2. Aktivera miljön
+    source .venv/bin/activate
+
+    # 3. Installera dina paket blixtsnabbt (uv sköter detta)
+    echo "Ser till att jupyturtle och jupyter är installerade..." | lolcat
+    uv pip install jupyter jupyturtle
+
+    echo "Startar Jupyter..."
+    jupyter notebook
+  '';
+}
